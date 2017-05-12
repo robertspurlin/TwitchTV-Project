e4d1ccff1b9f6d9477a923514530fa13998ad469
Robert Spurlin <rts3e@mtmail.mtsu.edu>
2017-05-12 13:35:40 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -1,54 +1,74 @@
-$(document).ready(function() {	
-    
-    var hCenter = (($(this).height() /2) - ($("#center").height() /2));
-    $("#center").css("margin-top", hCenter);
+var channels = ["ESL_SC2", "OgamingSC2", "freecodecamp"];
 
-    if (typeof html == "undefined") {
-        $(window).resize(function() {
-            var homeHeight = $(this).height();
-            $("#background").height(homeHeight);
-        }).resize(); 
-    }
+$(document).ready(function() {
+    getChannelInfo();
+
 });
-
-var url = "https://en.wikipedia.org/w/api.php?callback=?";
-var link, details, html = "";
 
 $("#search").keypress(function(e) {
     if (e.which == 13) {
-        getResults();
+        channels.push($("#search").val());
+        getChannelInfo();
     }
 });
 
-function getResults() {
-    
-    $.getJSON(url, {
-        "action": "query",
-        "format": "json",
-        "prop": "info|extracts",
-        "list": "",
-        "continue": "",
-        "titles": "",
-        "generator": "search",
-        "inprop": "url",
-        "exsentences": "3",
-        "exlimit": "max",
-        "exintro": 1,
-        "explaintext": 1,
-        "gsrsearch": "potato",
-        "gsrprop": "",
-        "gsrsearch": $("#search").val()
-    
-    }).done(function(response) {
-        $(".results").empty();
-        $("#center").css("margin-top", 0);
-        var pages = response.query.pages;
-        for (results in pages) {
-            link = "<a href='" + pages[results].fullurl + "'target='_blank'</a>";
-            details = "<h4>" + pages[results].title + "</h4><br /><p>" + pages[results].extract + "</p>";
-            html = link + "<div class='well'>" + details + "</div>"
-            $(".results").append(html);
-        };
-        $('#background').height($('#center').height());      
-      })
+$('body').on('click', '.remove', function(){
+    var index = channels.indexOf(this.id);
+    if (index > -1) {
+        channels.splice(index, 1);
+        getChannelInfo();
     }
+});
+
+var status, imageURL;
+
+function getChannelInfo() {
+    
+    $('#box').empty();  
+    
+    channels.forEach(function(channel) {
+
+        function makeURL(type, name) {
+            return 'https://wind-bow.gomix.me/twitch-api/' + type + '/' + name + '?callback=?';
+        };
+       
+        $.getJSON(makeURL("channels", channel), function(channelData) {
+            
+            $.getJSON(makeURL("streams", channel), function(streamData) {
+               
+                if (!channelData.error) {
+                    if (streamData.stream !== null) {
+                        status = "<a href='https://twitch.tv/" + channelData.name + "'target='_blank'>" + channelData.display_name + "</a> is online.</br>" + streamData.stream.channel.status;
+                        
+                        if (channelData.logo == null) {
+                            imageURL = "https://placehold.it/300x300?text=_";
+                        } else {
+                            imageURL = channelData.logo;
+                        }
+
+                        $('#box').append(
+                            "<div class='well' id='" + channel + "'style='background-color: #83f442;'><div class='row'><div class='col-xs-2'><img class='img-thumbnail remove' id='" + channel + "'src='" + imageURL +  "'/></div><div class='col-xs-10'><h5>" + status + "</h5></div></div></div>");
+                    } else {
+                        status = "<a href='https://twitch.tv/" + channelData.name + "'target='_blank'>" + channelData.display_name + "</a> is offline.";
+                        
+                        if (channelData.logo == null) {
+                            imageURL = "https://placehold.it/300x300?text=_";
+                        } else {
+                            imageURL = channelData.logo;
+                        }
+
+                        $('#box').append(
+                            "<div class='well' id='" + channel + "'><div class='row'><div class='col-xs-2'><img class='img-thumbnail remove' id='" + channel + "'src='" + imageURL +  "'/></div><div class='col-xs-10'><h5>" + status + "</h5></div></div></div>");
+                    }
+                } else {
+                    status = channel + " does not exist.";
+                    imageURL = "https://placehold.it/300x300?text=_";
+                    
+                        $('#box').append(
+                            "<div class='well' id='" + channel + "' style='background-color: white;'><div class='row'><div class='col-xs-2'><img class='img-thumbnail remove' id='" + channel + "'src='" + imageURL +  "'/></div><div class='col-xs-10'><h5>" + status + "</h5></div></div></div>");
+                }
+            });
+        });
+    });
+};
+
